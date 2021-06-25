c7232087d1b842554df3fea5f9cdbda446458550
ankurk10 <ankurkumarkushwaha.10@gmail.com>
2021-06-25 17:06:29 +0530

    Added Splash Screen and App Icon

---
+++ app/src/main/java/com/example/konnect/InternshipActivity.java
@@ -0,0 +1,14 @@
+package com.example.konnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InternshipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internship);
+    }
+}
