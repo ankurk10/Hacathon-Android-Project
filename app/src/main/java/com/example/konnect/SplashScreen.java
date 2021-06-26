2a7c55e8e1b8a05055c7126f1d8e4c6bd0f3c052
ankurk10 <ankurkumarkushwaha.10@gmail.com>
2021-06-26 13:45:11 +0530

    Fixed Splash Screen and Pomodoro Timer

--- app/src/main/java/com/example/konnect/SplashScreen.java
+++ app/src/main/java/com/example/konnect/SplashScreen.java
@@ -1,8 +1,11 @@
 package com.example.konnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -12,6 +15,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int startColor = getWindow().getStatusBarColor();
+            int endColor = ContextCompat.getColor(this, R.color.logincolor);
+            ObjectAnimator.ofArgb(getWindow(), "statusBarColor", startColor, endColor).start();
+        }
+
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
